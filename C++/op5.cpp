bbe1baff196cf507ff2254f1c4e6f53804e1b661
hyeonggeun0209 <enddj99@naver.com>
2024-07-29 16:18:12 +0900

    Power class with ++ prefix operator overloading

---
+++ C++/op5.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Power {
+private:
+    int kick;
+    int punch;
+public:
+    Power(int kick=0, int punch=0) {
+        this->kick = kick;
+        this->punch = punch;
+    }
+    void show();
+    Power& operator ++();
+};
+
+void Power::show() {
+    cout << "kick = " << kick << ", " << "punch = " << punch << endl;
+}
+
+Power& Power::operator++() {
+    kick ++;
+    punch ++;
+    return *this;
+    
+}
+
+int main() {
+    Power a(3,5), b;
+    a.show();
+    b.show();
+    b = ++a;
+    a.show();
+    b.show();
+
+    return 0;
+}
