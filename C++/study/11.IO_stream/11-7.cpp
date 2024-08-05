521df613944eaa4533aabe31c22bfa8b63d95807
hyeonggeun0209 <enddj99@naver.com>
2024-08-05 11:59:11 +0900

    Use manipulator without parameters

---
+++ C++/study/11.IO_stream/11-7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << hex << showbase << 30 << endl;
+    cout << dec << showpos << 100 << endl;
+
+    cout << true << ' ' << false << endl;
+    cout << boolalpha << true << ' ' << false << endl;
+
+    return 0;
+}
