11a4d2d753f973528d2c98d363d34f2017c85faa
hyeonggeun0209 <enddj99@naver.com>
2024-08-05 14:03:27 +0900

    HelloWorldApp

--- qt/HelloWorldApp/main.cpp
+++ qt/HelloWorldApp/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+
+    char password[11];
+    cout << "if you want quit, enter password" << endl;
+
+    while(true) {
+
+        cout << "password >> ";
+        cin >> password;
+
+        if(strcmp(password, "C++") == 0) {
+            cout << "quit" << endl;
+            break;
+        }
+        else
+            cout << "wrong password" << endl;
+    }
+
     return 0;
 }
