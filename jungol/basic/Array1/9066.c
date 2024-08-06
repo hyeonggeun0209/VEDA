94f611ec8069f42d8cea0dd992f9ec524f8fccce
hyeonggeun0209 <enddj99@naver.com>
2024-08-06 17:04:32 +0900

    Jungol basic array1

--- jungol/basic/Array1/9066.c
+++ jungol/basic/Array1/9066.c
@@ -2,10 +2,11 @@
 
 int main() {
     char arr[26];
+    char c = 'A';
     for (int i = 0; i < 26; i++) {
-        scanf(" %c", &arr[i]);
+        arr[i] = c++;
     }
-    for (int i = 25; i >=0; i++) {
+    for (int i = 25; i >=0; i--) {
         printf("%c ", arr[i]);
     }
     return 0;
