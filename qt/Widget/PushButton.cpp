f39691894939f9a0b6e72e52acdc20b3e8a0af87
hyeonggeun0209 <enddj99@naver.com>
2024-08-06 17:06:37 +0900

    PushButton widget

---
+++ qt/Widget/PushButton.cpp
@@ -0,0 +1,15 @@
+// #include "widget.h"
+// #include <QApplication>
+// #include <QPushButton>
+
+// int main(int argc, char *argv[])
+// {
+//     QApplication a(argc, argv);
+
+//     QPushButton *pushButton = new QPushButton("Push&Button", 0);
+//     pushButton->setCheckable(true);
+//     QObject::connect(pushButton, &QPushButton::toggled, [&](bool flag){ qDebug() << "toggled"; if(!flag) a.quit();});
+//     pushButton->show();
+
+//     return a.exec();
+// }
