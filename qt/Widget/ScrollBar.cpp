a394e77755e5a1961f877687236b68d4ad864a7b
hyeonggeun0209 <enddj99@naver.com>
2024-08-07 14:11:39 +0900

    QScrollBar widget

---
+++ qt/Widget/ScrollBar.cpp
@@ -0,0 +1,21 @@
+// #include "widget.h"
+// #include <QApplication>
+// #include <QScrollArea>
+// #include <QScrollBar>
+// #include <QLabel>
+
+// int main(int argc, char *argv[])
+// {
+//     QApplication a(argc, argv);
+
+//     QLabel *label = new QLabel("1234567890123456789012345678901234567890");
+//     QScrollArea* scrollArea = new QScrollArea;
+//     scrollArea->setBackgroundRole(QPalette::Dark);
+//     scrollArea->setWidget(label);
+//     scrollArea->resize(60, 30);
+//     scrollArea->show();
+//     QScrollBar *slider = scrollArea->horizontalScrollBar();
+//     slider->setValue(slider->maximum());
+
+//     return a.exec();
+// }
