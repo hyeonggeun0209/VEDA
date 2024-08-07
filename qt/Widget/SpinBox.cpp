5c87f53b3dea945dbf6f0245955cea4d21c6cd44
hyeonggeun0209 <enddj99@naver.com>
2024-08-07 14:11:04 +0900

    QSpinBox widget

---
+++ qt/Widget/SpinBox.cpp
@@ -0,0 +1,18 @@
+// #include "widget.h"
+// #include <QApplication>
+// #include <QSpinBox>
+
+// int main(int argc, char *argv[])
+// {
+//     QApplication a(argc, argv);
+
+//     QSpinBox *spinBox = new QSpinBox();
+//     spinBox->resize(100, 30);
+//     spinBox->setRange(0, 10);
+//     spinBox->setSuffix("%");
+//     spinBox->setWrapping(true);
+//     spinBox->setSpecialValueText("Percent");
+//     spinBox->show();
+
+//     return a.exec();
+// }
