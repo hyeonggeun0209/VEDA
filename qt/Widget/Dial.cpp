49b959f088ad7d0ec378f365ecf34de88e7c64f6
hyeonggeun0209 <enddj99@naver.com>
2024-08-07 14:11:18 +0900

    QDial widget

---
+++ qt/Widget/Dial.cpp
@@ -0,0 +1,16 @@
+// #include "widget.h"
+// #include <QApplication>
+// #include <QDial>
+
+// int main(int argc, char *argv[])
+// {
+//     QApplication a(argc, argv);
+
+//     QDial *dial = new QDial();
+//     dial->setWrapping(true);
+//     dial->setNotchesVisible(true);
+//     dial->setNotchTarget(10);
+//     dial->show();
+
+//     return a.exec();
+// }
