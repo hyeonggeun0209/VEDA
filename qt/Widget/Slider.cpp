ddd64f5a1e5f4f1f13fc84b10a14c1d1a90fd562
hyeonggeun0209 <enddj99@naver.com>
2024-08-08 15:00:38 +0900

    QSlider widget

--- qt/Widget/Slider.cpp
+++ qt/Widget/Slider.cpp
@@ -1,16 +1,16 @@
-// #include "widget.h"
-// #include <QApplication>
-// #include <QSlider>
-// #include <QStyleFactory>
+#include "widget.h"
+#include <QApplication>
+#include <QSlider>
+#include <QStyleFactory>
 
-// int main(int argc, char *argv[])
-// {
-//     QApplication a(argc, argv);
-//     QSlider *slider = new QSlider();
-//     slider->setOrientation(Qt::Horizontal);
-//     slider->setTickPosition(QSlider::TicksAbove);
-//     slider->setStyle(QStyleFactory::create("Fusion"));
-//     slider->show();
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QSlider *slider = new QSlider();
+    slider->setOrientation(Qt::Horizontal);
+    slider->setTickPosition(QSlider::TicksAbove);
+    slider->setStyle(QStyleFactory::create("Fusion"));
+    slider->show();
 
-//     return a.exec();
-// }
+    return a.exec();
+}
