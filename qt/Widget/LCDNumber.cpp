690a531a5a3c422fde0151fb4bdcce4c90cc782e
hyeonggeun0209 <enddj99@naver.com>
2024-08-08 14:09:36 +0900

    LCDNumber widget

--- qt/Widget/LCDNumber.cpp
+++ qt/Widget/LCDNumber.cpp
@@ -1,17 +1,17 @@
-// #include "widget.h"
-// #include <QLCDNumber>
-// #include <QApplication>
+#include "widget.h"
+#include <QLCDNumber>
+#include <QApplication>
 
-// int main(int argc, char *argv[])
-// {
-//     QApplication a(argc, argv);
-//     QLCDNumber *lcd = new QLCDNumber();
-//     lcd->setBinMode();
-//     lcd->setSegmentStyle(QLCDNumber::Flat);
-//     lcd->resize(300, 300);
-//     lcd->display(2002);
-//     lcd->setDigitCount(15);
-//     lcd->show();
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QLCDNumber *lcd = new QLCDNumber();
+    lcd->setBinMode();
+    lcd->setSegmentStyle(QLCDNumber::Flat);
+    lcd->resize(300, 300);
+    lcd->display(2002);
+    lcd->setDigitCount(15);
+    lcd->show();
 
-//     return a.exec();
-// }
+    return a.exec();
+}
