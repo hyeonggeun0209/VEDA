6498903fa67c6f3fc4d2ecbf72808637f539b649
hyeonggeun0209 <enddj99@naver.com>
2024-08-08 14:57:51 +0900

    QLineEdit widget

--- qt/Widget/LineEdit.cpp
+++ qt/Widget/LineEdit.cpp
@@ -1,29 +1,29 @@
-// #include "widget.h"
-// #include <QApplication>
-// #include <QLineEdit>
-// #include <QCompleter>
+#include "widget.h"
+#include <QApplication>
+#include <QLineEdit>
+#include <QCompleter>
 
-// int main(int argc, char *argv[])
-// {
-//     QApplication a(argc, argv);
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
 
-//     // QLineEdit widget
-//     QLineEdit *lineEdit = new QLineEdit();
-//     lineEdit->setEchoMode(QLineEdit::PasswordEchoOnEdit);
-//     lineEdit->setInputMask("000.000.000.000;_");
-//     lineEdit->setPlaceholderText("input the number");
-//     lineEdit->show();
+    // QLineEdit widget
+    QLineEdit *lineEdit = new QLineEdit();
+    lineEdit->setEchoMode(QLineEdit::PasswordEchoOnEdit);
+    lineEdit->setInputMask("000.000.000.000;_");
+    lineEdit->setPlaceholderText("input the number");
+    lineEdit->show();
 
-//     //QCompleter class
-//     QStringList wordList;
-//     wordList << "alpha" << "omega" << "omicron" << "zeta";
+    //QCompleter class
+    QStringList wordList;
+    wordList << "alpha" << "omega" << "omicron" << "zeta";
 
-//     QLineEdit *lineEdit = new QLineEdit();
+    QLineEdit *lineEdit = new QLineEdit();
 
-//     QCompleter *completer = new QCompleter(wordList, 0);
-//     completer->setCaseSensitivity(Qt::CaseInsensitive);
-//     lineEdit->setCompleter(completer);
-//     lineEdit->show();
+    QCompleter *completer = new QCompleter(wordList, 0);
+    completer->setCaseSensitivity(Qt::CaseInsensitive);
+    lineEdit->setCompleter(completer);
+    lineEdit->show();
 
-//     return a.exec();
-// }
+    return a.exec();
+}
