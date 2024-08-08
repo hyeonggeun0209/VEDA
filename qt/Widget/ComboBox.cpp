7fafa0da34c192d7eeb16d07c0d865c6bc2cd031
hyeonggeun0209 <gudrms9921@gmail.com>
2024-08-08 13:58:29 +0900

    QComboBox widget

--- qt/Widget/ComboBox.cpp
+++ qt/Widget/ComboBox.cpp
@@ -1,19 +1,19 @@
-// #include "widget.h"
-// #include <QApplication>
-// #include <QComboBox>
+#include "widget.h"
+#include <QApplication>
+#include <QComboBox>
 
-// int main(int argc, char *argv[])
-// {
-//     QApplication a(argc, argv);
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
 
-//     QComboBox *comboBox = new QComboBox();
-//     comboBox->addItem("KDE");
-//     comboBox->addItem("Gnome");
-//     comboBox->addItem("FVWM");
-//     comboBox->addItem("CDE");
-//     comboBox->setEditable(true);
-//     comboBox->show();
+    QComboBox *comboBox = new QComboBox();
+    comboBox->addItem("KDE");
+    comboBox->addItem("Gnome");
+    comboBox->addItem("FVWM");
+    comboBox->addItem("CDE");
+    comboBox->setEditable(true);
+    comboBox->show();
 
 
-//     return a.exec();
-// }
+    return a.exec();
+}
