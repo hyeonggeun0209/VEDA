1368f619682fbd67c905f3dc421ba769f214e9b2
hyeonggeun0209 <enddj99@naver.com>
2024-08-14 12:00:27 +0900

    HelloQt with widget ui

---
+++ qt/HelloQt/HelloQt_ko_KR.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="widget.ui" line="14"/>
+        <source>Widget</source>
+        <translation>위젯</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="57"/>
+        <source>&amp;Value</source>
+        <translation>값</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="68"/>
+        <source>99</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="71"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="77"/>
+        <source>Input number</source>
+        <translation>숫자를 입력하세요</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="116"/>
+        <source>&amp;Quit</source>
+        <translation>종료</translation>
+    </message>
+</context>
+</TS>
