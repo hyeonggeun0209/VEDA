6622b502e32af19f8e105daa4cdf75edd956c2d8
hyeonggeun0209 <enddj99@naver.com>
2024-09-05 10:10:26 +0900

    MiniProject ProductManager class

--- Project/example/productmanager.cpp
+++ Project/example/productmanager.cpp
@@ -10,7 +10,7 @@
 ProductManager::ProductManager()
 {
    ifstream file;
-   file.open("productlist.txt");
+   file.open("productlist.csv");
    if(!file.fail()) {
        while(!file.eof()) {
            vector<string> row = parseCSV(file, ',');
@@ -27,7 +27,7 @@ ProductManager::ProductManager()
 ProductManager::~ProductManager()
 {
    ofstream file;
-   file.open("productlist.txt");
+   file.open("productlist.csv");
    if(!file.fail()) {
       for (const auto& v : productList) {
           Product* c = v.second;
@@ -38,12 +38,12 @@ ProductManager::~ProductManager()
    file.close( );
 }
 
-void ProductManager::inputProduct( )
+void ProductManager::inputProduct()
 {
     string name, price;
     cout << "name : "; cin >> name;
     cout << "price : "; cin.ignore(); getline(cin, price, '\n'); //cin >> address;
-
+    
     int id = makeId( );
     Product* c = new Product(id, name, price);
     productList.insert( { id, c } );
@@ -126,45 +126,6 @@ vector<string> ProductManager::parseCSV(istream &file, char delimiter)
     return row;
 }
 
-
-bool ProductManager::displayMenu()
-{
-    int ch, key;
-    cout << "\033[2J\033[1;1H";
-    cout << "+++++++++++++++++++++++++++++++++++++++++++++" << endl;
-    cout << "                 Product Manager                 " << endl;
-    cout << "+++++++++++++++++++++++++++++++++++++++++++++" << endl;
-    cout << "  1. Display Product List                     " << endl;
-    cout << "  2. input Product                            " << endl;
-    cout << "  3. Delete Product                           " << endl;
-    cout << "  4. Modify Product                           " << endl;
-    cout << "  5. Quit Product Manager                       " << endl;
-    cout << "+++++++++++++++++++++++++++++++++++++++++++++" << endl;
-    cout << " What do you wanna do? ";
-    cin >> ch;
-    switch(ch) {
-        case 1: default:
-            displayInfo();
-            cin.ignore();
-            getchar();
-            break;
-        case 2:
-            inputProduct();
-            break;
-        case 3:
-            displayInfo();
-            cout << "   Choose Key : ";
-            cin >> key;
-            deleteProduct(key);
-            break;
-        case 4:
-            displayInfo();
-            cout << "   Choose Key : ";
-            cin >> key;
-            modifyProduct(key);
-            break;
-        case 5:
-            return false;
-    }
-    return true;
+int ProductManager::getsize() {
+    return productList.size();
 }
--- Project/example/productmanager.h
+++ Project/example/productmanager.h
@@ -4,27 +4,28 @@
 #include <map>
 #include <vector>
 #include "product.h"
-
+#include "stockmanager.h"
 using namespace std;
 
 class ProductManager {
-  public:
-      ProductManager();
-      ~ProductManager();
+    public:
+        ProductManager();
+        ~ProductManager();
 
-      void inputProduct( );
-      void addProduct(Product*);
-      void deleteProduct(int);
-      void modifyProduct(int);
-      Product* search(int);
-      int makeId();
-      void displayInfo();
-      vector<string> parseCSV(istream&, char);
+        void inputProduct();
+        void addProduct(Product*);
 
-      bool displayMenu();
+        void deleteProduct(int);
+        void modifyProduct(int);
+        Product* search(int);
+        int getsize();
+        int makeId();
+        void displayInfo();
+        vector<string> parseCSV(istream&, char);
 
-  private:
-      map<int, Product*> productList;
+    private:
+        map<int, Product*> productList; 
+
 };
 
 #endif    // __PRODUCT_MANAGER_H__
