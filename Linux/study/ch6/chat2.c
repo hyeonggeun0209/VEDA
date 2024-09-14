6775b9d0c8929fa22eb6efc2efe9b3bc1a6913e5
hyeonggeun0209 <enddj99@naver.com>
2024-09-14 12:16:38 +0900

    Linux study chapter 6

--- Linux/study/ch6/chat2.c
+++ Linux/study/ch6/chat2.c
@@ -6,14 +6,15 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define TCP_PORT 5100 				/* 서버의 포트 번호 */
+#define TCP_PORT 5200 				/* 서버의 포트 번호 */
 
-static int g_noc = 0;
 typedef struct client_sock {
   char ip[BUFSIZ];
   int s_id;
   int csfd;
 } c_sock;
+
+static int g_noc = 0;
 
 void sigfunc(int no) 
 {
@@ -23,82 +24,87 @@ void sigfunc(int no)
 
 int main(int argc, char **argv)
 {
-    int p[2];
-    pipe(p);
-    c_sock* cs[5];
-    int ssock, portno, n;                   /* 소켓 디스크립트 정의 */
-    struct sockaddr_in servaddr, cliaddr; 	/* 주소 구조체 정의 */
-    socklen_t clen;
-    pid_t pid;                              /* fork( ) 함수를 위한 PID */
-    char mesg[BUFSIZ];
+  int p[2];
+  pipe(p);
+  c_sock* cs[5];
+  int ssock, portno, n;                   /* 소켓 디스크립트 정의 */
+  int status;
+  struct sockaddr_in servaddr, cliaddr; 	/* 주소 구조체 정의 */
+  socklen_t clen;
+  pid_t pid;                              /* fork( ) 함수를 위한 PID */
+  char mesg[BUFSIZ];
 
-    portno = (argc == 2)?atoi(argv[1]):TCP_PORT;
+  portno = (argc == 2)?atoi(argv[1]):TCP_PORT;
 
-    signal(SIGCHLD, sigfunc);
+  signal(SIGCHLD, sigfunc);
 
-    /* 서버 소켓 생성 */
-    if((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket()");
-        return -1;
-    }
+  /* 서버 소켓 생성 */
+  if((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+      perror("socket()");
+      return -1;
+  }
 
-    /* 주소 구조체에 주소 지정 */
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(portno); 	/* 사용할 포트 지정 */
+  /* 주소 구조체에 주소 지정 */
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(portno); 	/* 사용할 포트 지정 */
 
-    /* bind 함수를 사용하여 서버 소켓의 주소 설정 */
-    if(bind(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        perror("bind()");
-        return -1;
-    }
+  /* bind 함수를 사용하여 서버 소켓의 주소 설정 */
+  if(bind(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    perror("bind()");
+    return -1;
+  }
 
-    /* 동시에 접속하는 클라이언트의 처리를 위한 대기 큐를 설정 */
-    if(listen(ssock, 8) < 0) {
-        perror("listen()");
-        return -1;
-    }
+  /* 동시에 접속하는 클라이언트의 처리를 위한 대기 큐를 설정 */
+  if(listen(ssock, 8) < 0) {
+    perror("listen()");
+    return -1;
+  }
 
-    clen = sizeof(cliaddr);
+  clen = sizeof(cliaddr);
+  int i = 0;
+
+  if((pid = fork()) < 0) {  
+    perror("Error");
+
+  } else if (pid == 0) {
     do {
-        /* 클라이언트가 접속하면 접속을 허용하고 클라이언트 소켓 생성 */
-        int csock = accept(ssock, (struct sockaddr *)&cliaddr, &clen);
+      int csock = accept(ssock, (struct sockaddr *)&cliaddr, &clen);
 
-        /* 네트워크 주소를 문자열로 변경 */
-        inet_ntop(AF_INET, &cliaddr.sin_addr, mesg, BUFSIZ);
-        printf("Client is connected : %s\n", mesg);
-        cs[g_noc] = (c_sock*)malloc(sizeof(c_sock));
-        strcpy(cs[g_noc]->ip,mesg);
-        cs[g_noc]->csfd = csock;
-        cs[g_noc]->s_id = g_noc;
-        g_noc++;
-        /* 연결되는 클라이언트와의 통신을 위한 자식 프로세스 생성 */
-        if((pid = fork()) < 0) {  
-          perror("Error");
-        } else if(pid == 0) {
-          do {
-            memset(mesg, 0, BUFSIZ);
-            if((n = read(csock, mesg, BUFSIZ)) <= 0)
-              perror("read()");
-            printf("(%d)Received data : %s", g_noc-1, mesg);
-            write(p[1], mesg, sizeof(mesg));
-          } while(strncmp(mesg, "q", 1));
-        } else {
-            while(g_noc != 0) {
-              n = read(p[0], mesg, sizeof(mesg));
-              printf("%d\n", g_noc);
-              for(int i = 0; i < g_noc; i++) {
-              if(write(cs[i]->csfd, mesg, n) <= 0) 
-                perror("write()");
-              }
-            }
-        }
-        // printf("Client is disconnected : %s\n", cs[0]->ip);
-        close(csock); 			/* 클라이언트 소켓을 닫음 */
+      /* 네트워크 주소를 문자열로 변경 */
+      inet_ntop(AF_INET, &cliaddr.sin_addr, mesg, BUFSIZ);
+      printf("Client is connected : %s\n", mesg);
+
+      cs[g_noc] = (c_sock*)malloc(sizeof(c_sock));
+      strcpy(cs[g_noc]->ip,mesg);
+      cs[g_noc]->csfd = csock;
+      cs[g_noc]->s_id = g_noc;
+      g_noc++;
+
+      if((pid = fork()) < 0) {  
+        perror("Error");
+      } else if(pid == 0) {
+        do {
+          memset(mesg, 0, BUFSIZ);
+          if((n = read(csock, mesg, BUFSIZ)) <= 0)
+            perror("read()");
+          printf("(%s)Received data : %s", cs[g_noc - 1]->ip, mesg);
+          write(p[1], mesg, sizeof(mesg));
+        } while(strncmp(mesg, "q", 1));
+      }
     } while(strncmp(mesg, "q", 1));
+  } else {
+    while(1) {
+      n = read(p[0], mesg, sizeof(mesg));
+      for(int i = 0; i < g_noc; i++) {
+      if(write(cs[i]->csfd, mesg, n) <= 0) 
+        perror("write()");
+      }
+    }
+    wait(&status);
+  }
+  close(ssock);
 
-    close(ssock); 			/* 서버 소켓을 닫음 */
-
-    return 0;
+  return 0;
 }
