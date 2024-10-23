0d0dcd3dc70552591a2cf50bb3a429e92f7abe76
hyeonggeun0209 <gudrms9921@gmail.com>
2024-10-23 10:29:52 +0900

    client widget.h

---
+++ Project/healthcare/qt/ClientApp/widget.h
@@ -0,0 +1,46 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include "signin.h"
+#include "mainwindow.h"
+#include "adminwindow.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void onLoginClicked();
+    void onLoginResponse();
+    void onSignInClicked();
+    void onLoginBackButtonClicked();
+    void onRegistrationSuccessful();
+
+private:
+    Ui::Widget *ui;
+    QLineEdit *idLineEdit;
+    QLineEdit *pwLineEdit;
+    QPushButton *loginButton;
+    QPushButton *signInButton;
+    login *loginWidget;
+    MainWindow *mainWindow;
+    AdminWindow *adminWindow;
+    QTcpSocket *socket;
+
+
+
+    bool validateLogin(const QString &id, const QString &password);
+};
+#endif // WIDGET_H
